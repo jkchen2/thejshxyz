aa26c883fb238f2fe8df31162617d26c35e495b8
jkchen2 <jkchen2@illinois.edu>
2017-07-13 19:35:32 -0500

    Auto-generated commit

---
+++ assets/disposition.js
@@ -0,0 +1,20 @@
+// this file is also pretty edgy
+// same warning as the thoughts file applies
+
+var dispositions = [
+"abject terror",
+"crippling depression",
+"crippling sadness",
+"ennui",
+"anxiety",
+"pure hatred",
+"tears",
+"insomnia",
+"poor diet",
+"dysphoria",
+"nihilism",
+"edginess",
+"despondence",
+"unemployment",
+"aggression"
+];
